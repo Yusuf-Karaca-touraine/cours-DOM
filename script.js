79e56ab8743ae14d95e9661b7424632b3bab4122
Yusuf Karaca <karacayif@gmail.com>
2025-05-19 16:53:20 +0200

    bears updated

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const sayMyName = prompt("Quel est votre nom ?");
-const title = document.querySelector(".title");
-title.textContent = `Hello, ${sayMyName}!`;
+// fichier script.js
+const bearsPictures = [
+    "https://placebear.com/500/279",
+    "https://placebear.com/500/280",
+    "https://placebear.com/500/300",
+    "https://placebear.com/500/302",
+    "https://placebear.com/500/305",
+  ];
+  // À toi de jouer ici 👇 
+
+  const picturesModified = document.querySelectorAll("img");
+  for (let i = 0; i < picturesModified.length; i++) {
+  picturesModified[i].src = bearsPictures[i];
+}
+
+// Ou sinon je fais une update version ternaire au cas où histoire de train le tout (à la maison)
+
+

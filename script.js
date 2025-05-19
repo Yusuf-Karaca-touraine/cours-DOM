1559856150d7993cf257e88b4d570d285a8ae541
Yusuf Karaca <karacayif@gmail.com>
2025-05-19 14:57:11 +0200

    cat really done this time

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const modifiedImg = document.querySelector(".img-cat").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDUWB51JwETzUH9_F2hZJzagg0LKEV6dYi8g&s";
-modifiedImg = document.querySelector (".img-cat") .width = 600;    
-modifiedImg = img.height = 400;   
+const modifiedImg = document.querySelector(".img-cat");
+modifiedImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDUWB51JwETzUH9_F2hZJzagg0LKEV6dYi8g&s";
+modifiedImg.width = 500;
+modifiedImg.height = 300;    
+
+const img = document.querySelector(".my-image");
+img.width = 300;    
+img.height = 200;

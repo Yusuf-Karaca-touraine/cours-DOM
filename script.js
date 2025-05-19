b78cbc8069b4c90c9e8b0bb6a223dd305fc36b38
Yusuf Karaca <karacayif@gmail.com>
2025-05-19 14:19:11 +0200

    img cat done

---
+++ script.js
@@ -0,0 +1,3 @@
+const modifiedImg = document.querySelector(".img-cat").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDUWB51JwETzUH9_F2hZJzagg0LKEV6dYi8g&s";
+modifiedImg = document.querySelector (".img-cat") .width = 600;    
+modifiedImg = img.height = 400;   

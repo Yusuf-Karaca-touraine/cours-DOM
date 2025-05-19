949108f4b1e5ad6fc6bae924fcdc70cb01d5b065
Yusuf Karaca <karacayif@gmail.com>
2025-05-19 16:23:21 +0200

    title html done

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const modifiedImg = document.querySelector(".img-cat");
-modifiedImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDUWB51JwETzUH9_F2hZJzagg0LKEV6dYi8g&s";
-modifiedImg.width = 500;
-modifiedImg.height = 300;    
-
-const img = document.querySelector(".my-image");
-img.width = 300;    
-img.height = 200;
+const sayMyName = prompt("Quel est votre nom ?");
+const title = document.querySelector(".title");
+title.textContent = `Hello, ${sayMyName}!`;

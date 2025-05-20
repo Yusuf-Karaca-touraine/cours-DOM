11b0aa79b70f1a79a57448ccfd4fa7d2245f42d9
Yusuf Karaca <karacayif@gmail.com>
2025-05-20 10:28:29 +0200

    Img of cat moved up

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const newCatImg = document.createElement("img");
-newCatImg.src = "https://cdn.britannica.com/39/226539-050-D21D7721/Portrait-of-a-cat-with-whiskers-visible.jpg";
-newCatImg.alt = "thinking cat";
-const secondCard = document.querySelector(".second-card");
-secondCard.appendChild(newCatImg);
+const newParent= document.getElementById("first-div");
+const catImgToMove = document.getElementById("second-cat-img");
+newParent.appendChild(catImgToMove);

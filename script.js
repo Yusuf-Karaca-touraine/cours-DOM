4fe016642ac536368c20a8a5eda804a73898cb17
Yusuf Karaca <karacayif@gmail.com>
2025-05-20 13:29:07 +0200

    Third box added

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const theBox = document.querySelector(".box");
-
-theBox.style.backgroundColor = "yellow";
-theBox.style.height = "300px";
-theBox.style.width = "400px";
-
-const theText = document.querySelector(".title");
-theText.style.fontSize = "60px";
-theText.style.textAlign = "center";
-theText.style.color = "lightblue";
+const newBox = document.createElement("div");
+newBox.classList.add("box");
+const container = document.querySelector(".container-boxes");
+container.appendChild(newBox);

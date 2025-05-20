54be09f72d53e89658aa03cd79421094a99030ab
Yusuf Karaca <karacayif@gmail.com>
2025-05-20 15:10:22 +0200

    dog and title changed by event

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const newBox = document.createElement("div");
-newBox.classList.add("box");
-const container = document.querySelector(".container-boxes");
-container.appendChild(newBox);
+const clickedImg = document.querySelector(".img-dogs");
+clickedImg.addEventListener("click", function() {
+    clickedImg.src = "https://placedog.net/499";
+    clickedImg.alt = "another dog";
+});
+
+const hoveredTitle = document.querySelector(".title");
+hoveredTitle.addEventListener("mouseover", () => { 
+    hoveredTitle.style.color = "green";
+});
+
+const unhoveredTitle = document.querySelector(".title");
+unhoveredTitle.addEventListener("mouseleave", () => {
+    unhoveredTitle.style.color = "black";
+});
+
+// Ou sinon en plus condensé si jamais tu veux faire plus clean
+
+/*
+["mouseover", "mouseleave"].forEach(event =>
+    document.querySelector(".title").addEventListener(event, e => {
+      e.target.style.color = event === "mouseover" ? "green" : "black";
+    })
+  );
+  */

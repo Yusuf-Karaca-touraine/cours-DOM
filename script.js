3c94ce7d65327437aac1888517c991d6c1e85ed5
Yusuf Karaca <karacayif@gmail.com>
2025-05-20 15:59:51 +0200

    Pokemon added

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-const clickedImg = document.querySelector(".img-dogs");
-clickedImg.addEventListener("click", function() {
-    clickedImg.src = "https://placedog.net/499";
-    clickedImg.alt = "another dog";
+const venusaurUrl = "https://pokeapi.co/api/v2/pokemon/3/";
+fetch(venusaurUrl)
+.then((response) => response.json())
+.then((pokemon) => {
+    document.body.innerHTML +=`
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.sprites.front_default}" alt="Halte la !"/>
+    `;
+    console.log(pokemon);
 });
-
-const hoveredTitle = document.querySelector(".title");
-hoveredTitle.addEventListener("mouseover", () => { 
-    hoveredTitle.style.color = "green";
-});
-
-const unhoveredTitle = document.querySelector(".title");
-unhoveredTitle.addEventListener("mouseleave", () => {
-    unhoveredTitle.style.color = "black";
-});
-
-// Ou sinon en plus condensé si jamais tu veux faire plus clean
-
-/*
-["mouseover", "mouseleave"].forEach(event =>
-    document.querySelector(".title").addEventListener(event, e => {
-      e.target.style.color = event === "mouseover" ? "green" : "black";
-    })
-  );
-  */

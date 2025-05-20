dbc349808afedda5ea6aac8fe6747a1bec58c3bc
Yusuf Karaca <karacayif@gmail.com>
2025-05-20 09:46:10 +0200

    Second img of cat done

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-// fichier script.js
-const bearsPictures = [
-    "https://placebear.com/500/279",
-    "https://placebear.com/500/280",
-    "https://placebear.com/500/300",
-    "https://placebear.com/500/302",
-    "https://placebear.com/500/305",
-  ];
-  // À toi de jouer ici 👇 
-
-  const picturesModified = document.querySelectorAll("img");
-  for (let i = 0; i < picturesModified.length; i++) {
-  picturesModified[i].src = bearsPictures[i];
-}
-
-// Ou sinon je fais une update version ternaire au cas où histoire de train le tout (à la maison)
-
-
+const newCatImg = document.createElement("img");
+newCatImg.src = "https://cdn.britannica.com/39/226539-050-D21D7721/Portrait-of-a-cat-with-whiskers-visible.jpg";
+newCatImg.alt = "thinking cat";
+const secondCard = document.querySelector(".second-card");
+secondCard.appendChild(newCatImg);
